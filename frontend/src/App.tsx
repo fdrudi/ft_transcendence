4f04c085146930be373bead49338557b5bb297a3
Wowbagger1994 <enrik.b94@live.it>
2023-03-29 17:56:25 +0200

    First commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
 
-function App() {
-  return <Button colorScheme="blue">Click me sempai UWU!</Button>;
-}
+const auth = async () => {
+  try {
+    return await fetch("/auth").then((res: any) => console.log(res));
+  } catch (error) {
+    console.error("Auth Error: ", error);
+  }
+  // fetch("/auth").then((res: any) => console.log(res));
+  return console.log("OKAY");
+};
+
+const App = () => {
+  return (
+    <Button onClick={auth} colorScheme="blue">
+      Click me sempai UWU!
+    </Button>
+  );
+};
 
 export default App;
