b1b3421c833994c653a31daf4ddead734f4ef0f3
Adrien <adrienrossignol1@gmail.com>
2023-03-31 18:54:48 +0200

    fix: FUCK REACT, GO NEXTJS

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import './index.tsx';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
